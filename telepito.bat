5ac49b9e80ee855d59582dc6d0c3e753341df57a
Kopitkó Péter <kopitko.peter@vjgy.hu>
2025-04-10 13:35:38 +0200

    Telepítő hibaelhárítás 04

--- telepito.bat
+++ telepito.bat
@@ -1,17 +1,24 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
+chcp 65001 > nul
 
-echo [1/3] Venv létrehozása...
+echo [1/5] Venv létrehozása...
 python -m venv venv
 
-echo [2/3] Venv aktiválása...
+echo [2/5] Venv aktiválása...
 call venv\Scripts\activate.bat
 
-echo [3/3] Könyvtárak telepítése...
+echo [3/5] pip frissítése...
+python -m pip install --upgrade pip
+
+echo [4/5] Követelmények telepítése...
 pip install -r my_final_project\requirements.txt
 
-echo Kész! A venv aktív.
-pause
-cls
+echo [5/5] Projekt indítása...
 
+echo ---
+echo ✅ Kész! A környezet aktív és a projekt fut.
+pause
+
+cls
 python my_final_project\indito.py
